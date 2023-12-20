46063f82f1ab59bf10d108416f2cbc48b781d3ce
medhat1234 <121037476+medhat1234@users.noreply.github.com>
2023-12-20 15:49:25 +0200

    Update compiled.js
    breaking uodates , new better input system, spawning zombies added, vector motion added (move towards)

--- theGame/compiled.js
+++ theGame/compiled.js
@@ -1,12 +1,6 @@
 let SW = window.innerWidth,
   SH = window.innerHeight;
-let pressedButtons = new Set();
-let mouse = {
-  position: {
-    x: 0,
-    y: 0,
-  },
-};
+
 let CSB = [false, false, false, false];
 const app = new PIXI.Application({
   height: SH,
@@ -16,69 +10,59 @@ const app = new PIXI.Application({
 
 app.renderer.view.style.position = "absolute";
 document.body.appendChild(app.view);
-let playerTexURL =
-    "https://lh3.googleusercontent.com/drive-viewer/AEYmBYRzbNkTua4JyWsGHwieNay_GchUw6EyS71CPs3jCVAYPqfN95gWSD1HA_I1nypxM-7Du27U16TAns9UBcr6SPvNI1XO7g=s2560",
-  projectileTexUrl =
-    "https://lh3.googleusercontent.com/drive-viewer/AEYmBYTKgkP40scMB8hK5UKB9nmqHWvJGLo4pTK95kqWsx9YQczw0IuBfMfdh0E-F1yQ4DylwhJbZWRm_vmC_zOO4CNXbaT2Nw=s2560";
+const playerTex = PIXI.Texture.from(
+  "https://lh3.googleusercontent.com/drive-viewer/AEYmBYSZ2EcfvWDYAmFdkgfzGahNcOp3lI9EnN4eiYy_jrglYYIxp80kX6fM8UJPJAKCCIGbzbJmi5DdkOrT7a-YzjMEd7PvOQ=s2560"
+);
+const ZombieTex = PIXI.Texture.from(
+  "https://lh3.googleusercontent.com/drive-viewer/AEYmBYQvyS9Tm8H7bYAxhb9JjnuoyCIF6CWHe0yeGp9MHVArYOmGu0YDjrf8RzZcppuAFExPP8__UnQTBc-Tdve6LT_3tS8egQ=s2560"
+);
+const bulletTex = PIXI.Texture.from(
+  "https://lh3.googleusercontent.com/drive-viewer/AEYmBYTGfbKfioP03I0I275dPKA74Db5-l0mmOSM1xUAF7923Dv4u730WdPb9j9f2gLJEttcH8rMmseaMI5igruwQiupNrys4A=s2560"
+);
+//const lightTex = PIXI.Texture.from(
+("https://e1.pngegg.com/pngimages/217/260/png-clipart-luces-round-white-blur-spot.png");
+//);
+//let light = PIXI.Sprite.from(lightTex);
+let player = PIXI.Sprite.from(playerTex);
+let zombies = [];
+let bullet = PIXI.Sprite.from(bulletTex);
 
-let player = PIXI.Sprite.from(playerTexURL);
-let bullet = PIXI.Sprite.from(projectileTexUrl),
-  isShooting = false,
-  dir = 0;
-let speed = 5,
-  shotspeed = 25;
-let bulletsArr = [];
+let speed = 3;
+
 app.stage.addChild(player);
-
 player.anchor.set(0.5);
 player.position.set(SW / 2, SH / 2);
 player.scale.set(0.3, 0.3);
-app.stage.eventMode = "static";
-app.stage.on("globalpointermove", (e) => {
-  mouse.position = e.data.global;
-});
-window.addEventListener("mousedown", (e) => {
-  console.log("shot");
-  shot(player, bullet);
-});
-window.addEventListener("mouseup", (e) => {
-  isShooting = false;
-});
-app.ticker.add(update);
 
+//add zombies
+for (i = 0; i < 20; i++) {
+  const zombie = PIXI.Sprite.from(ZombieTex);
+  zombie.anchor.set(0.5);
+  zombie.position.set(Math.random() * SW, Math.random() * SH);
+  zombie.scale.set(0.5 + Math.random() * 0.5);
+  app.stage.addChild(zombie);
+  zombies.push(zombie);
+}
+
+app.ticker.add(update);
 function update() {
   collided(player, SW, SH);
   movement();
-
-  player.rotation = LookAt2(player, mouse);
-  if (isShooting) {
-    particleSystem(dir, bulletsArr[bulletsArr.length - 1]);
+  Shooting();
+  player.rotation = lookAt(player, mouse);
+  zombies.forEach((e) => {
+    e.rotation = lookAt(e, player);
+    VectorMove(e, 1);
+  });
+}
+function Shooting() {
+  if (pressedButtons.has(0)) {
   }
 }
-function shot(shooter, bullet) {
-  bulletsArr.push(PIXI.Sprite.from(projectileTexUrl));
-  dir = shooter.rotation;
-  //intialize
-  app.stage.addChild(bulletsArr[bulletsArr.length - 1]);
-  bulletsArr[bulletsArr.length - 1].anchor.set(0.5);
-  bulletsArr[bulletsArr.length - 1].scale.set(0.7);
-  bulletsArr[bulletsArr.length - 1].position = shooter.position;
-  bulletsArr[bulletsArr.length - 1].rotation = shooter.rotation;
-  isShooting = true;
-}
-function particleSystem(direction, projectile) {
-  if (
-    projectile.position.x <= SW &&
-    projectile.position.x >= 0 &&
-    projectile.position.y <= SH &&
-    projectile.position.y >= 0
-  ) {
-    projectile.position.x -= shotspeed * Math.cos(Math.PI / 2 + direction);
-    projectile.position.y -= shotspeed * Math.sin(Math.PI / 2 + direction);
-  } else {
-    app.stage.removeChild(projectile);
-    bulletsArr.pop();
-  }
+function VectorMove(object, magnitude) {
+  //moves in constant direction in this case it moves in direction of object face
+  object.position.x -= magnitude * Math.cos(Math.PI / 2 + object.rotation);
+  object.position.y -= magnitude * Math.sin(Math.PI / 2 + object.rotation);
 }
 function movement() {
   //CSB = [0-collided right border,1-collided left border,2-collided lower border,3-collided upper border]
@@ -123,21 +107,12 @@ function movement() {
     }
   }
   if (pressedButtons.has(16)) {
-    speed = 10;
-  } else {
     speed = 5;
+  } else {
+    speed = 3;
   }
 }
-// input system
-window.addEventListener("keydown", (e) => {
-  if (e.repeat) {
-    return;
-  }
-  pressedButtons.add(e.keyCode);
-});
-window.addEventListener("keyup", (e) => {
-  pressedButtons.delete(e.keyCode);
-});
+
 function collided(player, screenwidth, screenheight) {
   if (player.position.x >= screenwidth - player.width / 2) {
     CSB[0] = true;
@@ -160,13 +135,48 @@ function collided(player, screenwidth, screenheight) {
     CSB[2] = false;
   }
 }
-function LookAt2(main, target) {
+
+function lookAt(main, target) {
   //return angle between two points
-  return (
-    Math.PI / 2 +
-    Math.atan2(
-      target.position.y - main.position.y,
-      target.position.x - main.position.x
-    )
-  );
+  return Math.PI / 2 + Math.atan2(target.y - main.y, target.x - main.x);
 }
+
+///////////********/////// input system  /////*********////////
+let pressedButtons = new Set();
+let mouse = {
+  x: 0,
+  y: 0,
+};
+
+//mouse
+
+window.addEventListener("pointermove", (e) => {
+  mouse.x = e.pageX;
+  mouse.y = e.pageY;
+});
+
+window.addEventListener("mousedown", (e) => {
+  pressedButtons.add(0);
+});
+window.addEventListener("mouseup", (e) => {
+  if (e.button === 2) {
+    pressedButtons.delete(1);
+  } else {
+    pressedButtons.delete(0);
+  }
+});
+
+window.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  pressedButtons.add(1);
+});
+
+//keyboard
+window.addEventListener("keydown", (e) => {
+  if (!e.repeat) {
+    pressedButtons.add(e.keyCode);
+  }
+});
+window.addEventListener("keyup", (e) => {
+  pressedButtons.delete(e.keyCode);
+});
